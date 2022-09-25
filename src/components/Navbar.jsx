50e1daded15aac52bd8ba20f42133913741e2702
arifbilir <arifbilirde@gmail.com>
2022-09-25 18:27:59 +0200

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React, {useState} from 'react'
+import{MdOutlineMenu } from "react-icons/md";
 
 const Navbar = () => {
-  return (
-    <div>Navbar</div>
-  )
-}
+  const [toggleBar, setToggleBar] = useState(false);
 
-export default Navbar
+  return (
+    <>
+    <div className='bg-[#212529] flex justify-between sm:justify-around text-white items-center'>
+      <div className='text-xl ml-5 sm:ml-0'>Alpha Tech</div>
+      <div>
+        <div className='sm:hidden mr-5 p-2 text-3xl cursor-pointer'
+        onClick={ () => setToggleBar(!toggleBar)}
+        >
+          <MdOutlineMenu/>
+          </div>
+          {console.log(toggleBar)}
+        <ul className='sm:flex hidden'>
+          {["Services", "Portfolio", "About Us", "Staff", "Contact Us"].map(
+            (item,idx) => (
+            <li 
+            key={idx} 
+            className="p-2 text-gray-400 hover:text-gray-300 duration-300">
+              <a href='!#'> {item} </a>
+            </li>
+
+          ))}
+        </ul>
+      </div>
+     </div>
+     {toggleBar && (
+      <ul className='sm:flex'>
+          {["Services", "Portfolio", "About Us", "Staff", "Contact Us"].map(
+            (item,idx) => (
+            <li 
+            key={idx} 
+            className="p-2 text-gray-400 hover:text-gray-300 duration-300">
+              <a href='!#'> {item} </a>
+            </li>
+            )
+          )}
+      </ul>
+      )}
+    </>
+  );
+};
+
+export default Navbar;
