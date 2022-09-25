daa36da6727e973ea70b9e3a521f454de9239fd6
arifbilir <arifbilirde@gmail.com>
2022-09-25 20:22:15 +0200

    done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,45 +1,50 @@
-import React, {useState} from 'react'
-import{MdOutlineMenu } from "react-icons/md";
+import React, { useState } from "react";
+import { MdOutlineMenu } from "react-icons/md";
 
 const Navbar = () => {
   const [toggleBar, setToggleBar] = useState(false);
 
   return (
     <>
-    <div className='bg-[#212529] flex justify-between sm:justify-around text-white items-center'>
-      <div className='text-xl ml-5 sm:ml-0'>Alpha Tech</div>
-      <div>
-        <div className='sm:hidden mr-5 p-2 text-3xl cursor-pointer'
-        onClick={ () => setToggleBar(!toggleBar)}
-        >
-          <MdOutlineMenu/>
+      <div className="bg-[#212529] flex justify-between sm:justify-around text-white items-center py-3">
+        <div className="text-xl ml-5 sm:ml-0">Alpha Tech</div>
+        <div>
+          <div
+            className="sm:hidden mr-5 p-2 text-3xl cursor-pointer"
+            onClick={() => setToggleBar(!toggleBar)}
+          >
+            <MdOutlineMenu />
           </div>
           {console.log(toggleBar)}
-        <ul className='sm:flex hidden'>
-          {["Services", "Portfolio", "About Us", "Staff", "Contact Us"].map(
-            (item,idx) => (
-            <li 
-            key={idx} 
-            className="p-2 text-gray-400 hover:text-gray-300 duration-300">
-              <a href='!#'> {item} </a>
-            </li>
-
-          ))}
-        </ul>
+          <ul className="sm:flex hidden">
+            {["Services", "Portfolio", "About Us", "Staff", "Contact Us"].map(
+              (item, idx) => (
+                <li
+                  key={idx}
+                  className="p-2 text-gray-400 hover:text-gray-300 duration-300"
+                >
+                  <a href="!#"> {item} </a>
+                </li>
+              )
+            )}
+          </ul>
+        </div>
       </div>
-     </div>
-     {toggleBar && (
-      <ul className='sm:flex'>
-          {["Services", "Portfolio", "About Us", "Staff", "Contact Us"].map(
-            (item,idx) => (
-            <li 
-            key={idx} 
-            className="p-2 text-gray-400 hover:text-gray-300 duration-300">
-              <a href='!#'> {item} </a>
-            </li>
-            )
-          )}
-      </ul>
+      {toggleBar && (
+        <div className="bg-[#212529] pb-2 sm:hidden">
+          <ul className="ml-[.7rem]">
+            {["Services", "Portfolio", "About Us", "Staff", "Contact Us"].map(
+              (item, idx) => (
+                <li
+                  key={idx}
+                  className="p-2 text-gray-400 hover:text-gray-300 duration-300"
+                >
+                  <a href="!#"> {item} </a>
+                </li>
+              )
+            )}
+          </ul>
+        </div>
       )}
     </>
   );
